617e3c677cd57fbf893530dd6310544d17fa304e
akhin <akin_ocal@hotmail.com>
2016-07-08 16:01:55 +0100

    making find filter a variable

--- source_code_formatter.sh
+++ source_code_formatter.sh
@@ -1,5 +1,7 @@
 #/bin/bash
-SED_ARGUMENT="-i" # -i.orig in order to save original files
+SED_ARGUMENT='-i' # Replace with -i.orig in order to save original files
+FIND_SEARCH_DIRECTORY='.'
+FIND_FILE_FILTER='-name \*.cpp -o -name \*.hpp -o -name \*.h;'
 
 IS_COMMAND_VALID_RESULT=0
 function is_command_valid()
@@ -7,7 +9,6 @@ function is_command_valid()
     local command_name=$1
     local which_command=`which ${command_name}`
     local valid_command=${#which_command}
-    
     if [ $valid_command -eq 0 ]; then
         IS_COMMAND_VALID_RESULT=0   
         return
@@ -28,22 +29,22 @@ function check_dos2unix()
 
 function convert_end_of_lines()
 {
-    find . -name \*.cpp -o -name \*.hpp -o -name \*.h | xargs dos2unix
+    eval find ${FIND_SEARCH_DIRECTORY} ${FIND_FILE_FILTER} | xargs dos2unix
 }
 
 function convert_tabs_to_spaces()
 {
-    find . -name \*.cpp -o -name \*.hpp -o -name \*.h | xargs sed ${SED_ARGUMENT} $'s/\t/    /g'
+    eval find ${FIND_SEARCH_DIRECTORY} ${FIND_FILE_FILTER} | xargs sed ${SED_ARGUMENT} $'s/\t/    /g'
 }
 
 function remove_trailing_whitespace()
 {
-    find . -name \*.cpp -o -name \*.hpp -o -name \*.h | xargs sed  ${SED_ARGUMENT} '' -e's/[ \t]*$//'
+    eval find ${FIND_SEARCH_DIRECTORY} ${FIND_FILE_FILTER} | xargs sed  ${SED_ARGUMENT} '' -e's/[ \t]*$//'
 }
 
 check_dos2unix
-echo "Formatting started\n"
+echo "Formatting started"
 convert_end_of_lines
 convert_tabs_to_spaces
 remove_trailing_whitespace
-echo "Formatting completed\n"
+echo "Formatting completed"
