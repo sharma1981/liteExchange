24db77bb82c9325f403e6a26b68746969e081474
akhin <akin_ocal@hotmail.com>
2016-08-09 23:34:44 +0100

    correcting gnu libc malloc-free hooking

--- source/memory/debugging/hook_gnu_libc.h
+++ source/memory/debugging/hook_gnu_libc.h
@@ -12,29 +12,51 @@ namespace debugging
 // http://www.gnu.org/software/libc/manual/html_node/Hooks-for-Malloc.html
 #include <malloc.h>
 
-inline void init_hook (void);
-static inline void *gnu_libc_malloc_hook (std::size_t, const void *);
-static inline void gnu_libc__free_hook (void*, const void *);
+inline void initHook (void);
+static inline void *gnuLibcMallocHook (std::size_t, const void *);
+static inline void gnuLibcFreeHook (void*, const void *);
 
 // Uncomment the next line to set the hooks
-void(*__malloc_initialize_hook) (void) = init_hook;
+void(*__malloc_initialize_hook) (void) = initHook;
 
-inline void init_hook(void)
+inline void initHook(void)
 {
     old_malloc_hook = __malloc_hook;
     old_free_hook = __free_hook;
-    __malloc_hook = my_malloc_hook;
-    __free_hook = my_free_hook;
+    __malloc_hook = gnuLibcMallocHook;
+    __free_hook = gnuLibcFreeHook;
 }
 
-static inline void *gnu_libc_malloc_hook(std::size_t size, const void *caller)
+static inline void *gnuLibcMallocHook(std::size_t size, const void *caller)
 {
-
+    void *result{nullptr};
+    // Restore all old hooks
+    __malloc_hook = old_malloc_hook;
+    __free_hook = old_free_hook;
+	
+    // Save underlying hooks
+    old_malloc_hook = __malloc_hook;
+    old_free_hook = __free_hook;
+    // Restore our own hooks
+    __malloc_hook = gnuLibcMallocHook;
+    __free_hook = gnuLibcFreeHook;
+    return result;
 }
 
-static inline void gnu_libc__free_hook(void*, const void * caller)
+static inline void gnuLibcFreeHook(void*, const void * caller)
 {
-
+    // Restore all old hooks
+    __malloc_hook = old_malloc_hook;
+    __free_hook = old_free_hook;
+    
+    
+    // Save underlying hooks
+    old_malloc_hook = __malloc_hook;
+    old_free_hook = __free_hook;
+  
+	// Restore our own hooks
+    __malloc_hook = gnuLibcMallocHook;
+    __free_hook = gnuLibcFreeHook;
 }
 
 
