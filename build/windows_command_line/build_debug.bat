0745757d2150d29e17c48c64e9d3b7252f41773f
akhin <akin_ocal@hotmail.com>
2016-08-31 07:54:53 +0100

    adding windows commandline based build scripts

---
+++ build/windows_command_line/build_debug.bat
@@ -0,0 +1,16 @@
+@echo off
+cls
+Color 0E
+SET SOLUTION=.\ome.sln
+REM For VS 2013 commnad prompt
+call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86_amd64
+REM For VS 2015 command prompt
+REM call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86_amd64
+REM
+REM Arrange working directory
+cd %~dp0
+cd ..
+cd windows
+CD
+msbuild %SOLUTION% /p:Configuration=Debug /property:Platform=Win32
+pause
