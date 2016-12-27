8a86b03bf26cba64ba4044dd13186a421ae364ea
akhin <akin_ocal@hotmail.com>
2016-12-27 11:59:06 +0000

    correcting spsc lockfree queue , it was not using the buffer to full capacity, if capacity is N, it was able to push only N-1 elements

--- source/concurrent/ring_buffer_spsc_lockfree.hpp
+++ source/concurrent/ring_buffer_spsc_lockfree.hpp
@@ -34,10 +34,11 @@ class RingBufferSPSCLockFree : public boost::noncopyable
         {
             const auto current_tail = m_write.load();
             const auto next_tail = increment(current_tail);
-            if (next_tail != m_read.load(std::memory_order_acquire))
+            
+			if (current_tail - m_read.load(std::memory_order_acquire) <= m_capacity -1 )
             {
-                m_buffer.get()[current_tail] = val;
-                m_write.store(next_tail, std::memory_order_release);
+                m_buffer.get()[current_tail % m_capacity ] = val;
+                m_write.store(next_tail , std::memory_order_release);
                 return true;
             }
 
@@ -51,24 +52,23 @@ class RingBufferSPSCLockFree : public boost::noncopyable
 
         bool tryPop(T* element)
         {
-            auto currentHead = m_read.load();
+            const auto currentHead = m_read.load();
 
-            if (currentHead == m_write.load(std::memory_order_acquire))
+            if (currentHead != m_write.load(std::memory_order_acquire))
             {
-                return false;
+				*element = m_buffer.get()[currentHead % m_capacity];
+				m_read.store(increment(currentHead) , std::memory_order_release);
+				return true;
             }
 
-            *element = m_buffer.get()[currentHead];
-            m_read.store(increment(currentHead), std::memory_order_release);
-
-            return true;
+			return false;
         }
 
     private:
 
         int increment(int n)
         {
-            return (n + 1) % m_capacity;
+			return (n + 1);
         }
 
         std::atomic<int> m_write;
